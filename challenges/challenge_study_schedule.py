80d42665fdd7832202df969f6e6b3afcf25ea43a
Rodrigo Lima <limarodrigoo@outlook.com>
2022-08-30 15:03:52 -0300

    feat: solve first challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if target_time is None:
+        return None
+    presence = 0
+    for period in permanence_period:
+        if type(period[0]) is not int or type(period[1]) is not int:
+            return None
+        if period[0] <= target_time <= period[1]:
+            presence += 1
+    return presence
