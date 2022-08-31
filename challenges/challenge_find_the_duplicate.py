7c2d2476a018364934b03a48483d1437518320c5
Rodrigo Lima <limarodrigoo@outlook.com>
2022-08-31 00:05:52 -0300

    feat: solve fourth challenge

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,8 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
+    nums.sort()
+    for index in range(len(nums) - 1):
+        if type(nums[index]) is not int or nums[index] < 1:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+    return False
