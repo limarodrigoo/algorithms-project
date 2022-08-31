320d5d7b363e08173d10863687b90423cbaafd0c
Rodrigo Lima <limarodrigoo@outlook.com>
2022-08-31 00:30:01 -0300

    feat: solve fifth challenge

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
+    if word == "":
+        return False
+    n = len(word) - 1
+    word = word.lower()
+    for index, char in enumerate(word):
+        if index >= n / 2:
+            return True
+        if char is not word[n - index]:
+            return False
+    return True
